f328a2cd5553b054c41265774062dc23d8db8d61
Felix Idalia <flxidalia@gmail.com>
2023-09-02 19:15:45 +0000

    Added variables.c file

---
+++ basic_concepts/variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Example how different variables are printed.
+ *
+ * Return: success (0)
+ */
+int main(void)
+{
+	int myNum = 15;
+	float myFloatNum = 5.99;
+	char myLetter = 'D';
+
+	printf("%d\n", myNum);
+	printf("%f\n", myFloatNum);
+	printf("%c\n", myLetter);
+
+	return (0);
+}
