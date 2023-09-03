d3fb21383b379865df00df25faf76a5b4cd3057b
Felix Idalia <flxidalia@gmail.com>
2023-09-03 08:28:09 +0000

    Declare multiple variables

---
+++ basic_concepts/0x04.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Declarring Mu;tiple Variables
+ *
+ * Return: 0 (success).
+ *
+ */
+int main(void)
+{
+	int x = 5, y = 6, z = 50;
+
+	printf("%d", x + y + z);
+	return (0);
+}
