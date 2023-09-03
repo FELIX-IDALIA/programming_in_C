307b509bf576639c672c1e5fc8978736176b3a4e
Felix Idalia <flxidalia@gmail.com>
2023-09-03 06:40:57 +0000

    Added 0x03.c file

---
+++ basic_concepts/0x03.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - starting point.
+ *
+ * Return: 0 (success).
+ *
+ */
+int main(void)
+{
+	/**
+	 * To print different types in a single printf() function,
+	 * you can use the following
+	 */
+	int age = 18;
+	int year = 4;
+	char grade = 'A';
+
+	printf("Age = %d.\nYear = %dth.\nGrade = %c.", age, year, grade);
+	return (0);
+}
