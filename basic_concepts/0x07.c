8a89f25250782af12ad0e7143260c1eefaacc8bd
Felix Idalia <flxidalia@gmail.com>
2023-09-06 07:38:31 +0000

    C type conversion

---
+++ basic_concepts/0x07.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - type conversion.
+ *
+ * Return: success (0)
+ */
+int main(void)
+{
+	int x = 5;
+	int y = 2;
+	int sum = x / y;
+
+	printf("%d", sum);
+	return (0);
+}
