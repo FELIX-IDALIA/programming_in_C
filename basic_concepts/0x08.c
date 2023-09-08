d7925791197e787cdd6d6e34d17d5034f0b57069
Felix Idalia <flxidalia@gmail.com>
2023-09-08 17:20:10 +0000

    Added file, 0x08.c

---
+++ basic_concepts/0x08.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Implicit conversion
+*
+* Return: success (0)
+*/
+
+int main(void)
+{
+
+/*Automatic conversion*/
+float myFloat = 11;
+
+printf("%f", myFloat); /* 11.000000 */
+return (0);
+
+}
