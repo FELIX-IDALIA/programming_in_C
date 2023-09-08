3fdbd64a2f5468ea551cb722a09e26c6f830ac49
Felix Idalia <flxidalia@gmail.com>
2023-09-08 17:27:41 +0000

    Added file, 0x09.c

---
+++ basic_concepts/0x09.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Automatic conversion: float to int.
+ *
+ * Return: success (0)
+ */
+int main(void)
+{
+	int myInt = 9.99;
+
+	printf("%d", myInt);
+	return (0);
+}
