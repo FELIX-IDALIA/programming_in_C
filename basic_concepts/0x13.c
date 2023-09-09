1fa03b42d13e4505d629580b789a961098368559
Felix Idalia <flxidalia@gmail.com>
2023-09-09 16:58:52 +0000

    Added file, 0x13.c

---
+++ basic_concepts/0x13.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+/**
+ * main - true or false
+ *
+ * Return: success (0)
+ */
+int main(void)
+{
+	/* Create boolean varibles */
+	bool isProgrammingFun = true;
+	bool isUgaliTasty = false;
+
+	/* Return boolean values */
+	printf("%d\n", isProgrammingFun); /* Returns 1(true) */
+	printf("%d\n", isUgaliTasty); /* Returns 0(false) */
+	return (0);
+}
