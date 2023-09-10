4d972551366cb51318c77cb29fa9e7621420cd82
Felix Idalia <flxidalia@gmail.com>
2023-09-10 17:10:17 +0000

    Added a file: 0x14.c

---
+++ basic_concepts/0x14.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Comparing values
+ *
+ * Return: success (0)
+ */
+int main(void)
+{
+	/* Returns 1 (True) beacuse 10 is greater than 9 */
+	printf("Returns :%d", 10 > 9);
+	return (0);
+}
