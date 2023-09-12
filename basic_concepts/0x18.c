2a31d9a51ee5ec5634f86691d7b6be3e47b84ee3
Felix Idalia <flxidalia@gmail.com>
2023-09-12 19:50:20 +0000

    Added a file: 0x18.c

---
+++ basic_concepts/0x18.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - if myAge is greater than or equal to 18
+ *
+ * Return: success (0)
+ */
+int main(void)
+{
+	int myAge = 25;
+	int votingAge = 18;
+
+	if (myAge >= votingAge)
+	{
+		printf("He/she is old enough to vote!");
+	}
+	else
+	{
+		printf("Not old enough to vote");
+	}
+	return (0);
+}
