a958ffb199a7bc97d20e0204bf8c139532377adb
Felix Idalia <flxidalia@gmail.com>
2023-09-15 20:21:04 +0000

    Adde a file; 0x19.c

---
+++ basic_concepts/0x19.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - if else conditions
+ *
+ * Return: success (0)
+ */
+int main(void)
+{
+	if (20 > 18)
+	{
+		printf("20 is greater than 18");
+	}
+	return (0);
+}
