ff228f57d60b01e2d9655bcc95141dcc5ae87ed3
Felix Idalia <flxidalia@gmail.com>
2023-09-16 19:03:08 +0000

    Added a file; 0x21.c

---
+++ basic_concepts/0x21.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: success (0).
+ */
+int main(void)
+{
+	int time = 20;
+
+	if (time < 18)
+	{
+		printf("Good day.");
+	}
+	else
+	{
+		printf("Good evening.");
+	}
+	return (0);
+}
+
