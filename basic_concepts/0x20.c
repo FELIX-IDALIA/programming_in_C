e43c1b788fc1c796d0f25b7c52ddbe9c96f127a7
Felix Idalia <flxidalia@gmail.com>
2023-09-16 18:52:15 +0000

    Added a file; 0x20.c

---
+++ basic_concepts/0x20.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: success (0).
+ */
+int main(void)
+{
+	int x = 20;
+	int y = 18;
+
+	if (x > y)
+	{
+		printf("x is greater than y");
+	}
+	return (0);
+}
