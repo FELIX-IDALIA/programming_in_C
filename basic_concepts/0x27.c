e3e0ca5c13aaa25b74fead16ae4c29d9ce696728
Felix Idalia <flxidalia@gmail.com>
2023-09-22 11:33:14 +0000

    Added a file (while loop)

---
+++ basic_concepts/0x27.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point, while loop
+ *
+ * Return: success (0).
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 5)
+	{
+		printf("%d\n", i);
+		i++;
+	}
+	return (0);
+}
