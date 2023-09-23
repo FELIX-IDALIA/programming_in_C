18740b4acd8837bf66b4d90c49e038b7b13619f0
Felix Idalia <flxidalia@gmail.com>
2023-09-23 11:52:30 +0000

    Added a file, do...while loop example

---
+++ basic_concepts/0x28.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point, do...while loop.
+ *
+ * Return: success (0).
+ */
+int main(void)
+{
+	int i = 0;
+
+	do {
+	printf("Output is: %d\n", i);
+	i++;
+	} while (i  < 5);
+
+	return (0);
+}
