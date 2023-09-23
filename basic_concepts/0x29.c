3a412227e879cc80e5b09ead7d19f8d84f00e86c
Felix Idalia <flxidalia@gmail.com>
2023-09-23 12:12:26 +0000

    File 0x29.c, do...while loop, testing for false condition

---
+++ basic_concepts/0x29.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Starting point.
+ *
+ * Return: success (0)
+ */
+int main(void)
+{
+	int i = 6;
+
+	do {
+		printf("Output is: %d\n", i);
+		i++;
+	} while (i < 5);
+
+	return (0);
+}
