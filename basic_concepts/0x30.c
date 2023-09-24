701872902140fd318c6cb311eb72da0df1bb5897
Felix Idalia <flxidalia@gmail.com>
2023-09-24 08:19:40 +0000

    Added a file(0x30.c) which explains for...loop

---
+++ basic_concepts/0x30.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: success (0)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (; i < 5; i++)
+	{
+	printf("%d\n", i);
+	}
+
+	return (0);
+}
