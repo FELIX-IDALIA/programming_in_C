591594bb9d67996781943e76e6527c42490481ae
Felix Idalia <flxidalia@gmail.com>
2023-09-24 12:40:30 +0000

    File 0x33.c demonstarting nested loop

---
+++ basic_concepts/0x33.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - nested loop\
+ *
+ * Return: success (0).
+ */
+int main(void)
+{
+	int i, j;
+
+	/* Outer loop */
+	for (i = 1; i <= 2; i++)
+	{
+		printf("Outer: %d\n", i); /* Executes 2 times. */
+
+	/* Inner loop */
+		for (j = 1; j <= 3; j++)
+		{
+			printf(" Inner: %d\n", j); /* Executes 6 times (2 * 3) */
+		}
+	}
+	return (0);
+}
