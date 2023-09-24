c6bc08b373061e14da25ccde0464beb645cfb5e2
Felix Idalia <flxidalia@gmail.com>
2023-09-24 11:02:10 +0000

    File demontrating for...loop, prints only even numbers from 0 to 10

---
+++ basic_concepts/0x32.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: success (0)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (; i <= 10; i += 2)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
