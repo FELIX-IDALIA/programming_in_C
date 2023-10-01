4d98c5b38bc01069c742810243b8d345513c2205
Felix Idalia <flxidalia@gmail.com>
2023-10-01 18:39:15 +0000

    Example, set array size

---
+++ basic_concepts/0x39.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: success (0)
+ */
+int main(void)
+{
+	/* Declare an array of four integers */
+	int arr[4];
+
+	/* Add elements */
+	arr[0] = 0;
+	arr[1] = 10;
+	arr[2] = 20;
+	arr[3] = 30;
+	arr[4] = 40;
+
+	printf("Index 0 output is: %d", arr[0]);
+
+	return (0);
+}
