c258006c3df4abbc5118d0a8224ac6d875583c53
Felix Idalia <flxidalia@gmail.com>
2023-10-04 19:00:12 +0000

    Example, working with Strings

---
+++ basic_concepts/0x43.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: success (0)
+ */
+int main(void)
+{
+	char greetings[] = "Hello World";
+
+	printf("Greeting is: %s", greetings);
+	return (0);
+}
