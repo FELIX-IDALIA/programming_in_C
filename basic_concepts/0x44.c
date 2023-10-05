79f48c7833363f09239744e95a84e717a3eaa795
Felix Idalia <flxidalia@gmail.com>
2023-10-05 17:29:07 +0000

    This example prints the first character (0) in greetings

---
+++ basic_concepts/0x44.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Return: success (0).
+ */
+int main(void)
+{
+	char greetings[] = "Hello World!";
+
+	printf("Character is: %c", greetings[0]);
+	return (0);
+}
