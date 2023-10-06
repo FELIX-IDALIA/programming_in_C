3ea6b68e23bbbfe61024c53b2d87c2cb0b4f93ff
Felix Idalia <flxidalia@gmail.com>
2023-10-06 19:59:28 +0000

    Another way of creating strings

---
+++ basic_concepts/0x47.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ *
+ * Return: success (0).
+ */
+int main(void)
+{
+	char name[] = {'F', 'E', 'L', 'I', 'X', '\0'};
+
+	printf("Name is: %s", name);
+	return (0);
+}
