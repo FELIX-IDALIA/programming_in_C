1fda139009fb79cbd70714f66f408777d05a82e2
Felix Idalia <flxidalia@gmail.com>
2023-10-08 11:52:06 +0000

    To get the length of a string, you can use the strlen() function

---
+++ basic_concepts/0x51.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point.
+ *
+ * Return: success (0).
+ */
+int main(void)
+{
+	char alphabets[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int len = strlen(alphabets);
+
+	printf("The length of string is: %d", len);
+	return (0);
+}
