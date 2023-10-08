1fee9f3d5eb702a0ed71be5afaf073e5774d1a4d
Felix Idalia <flxidalia@gmail.com>
2023-10-08 12:24:02 +0000

    sizeof and strlen behaves differently, as sizeof also includes the \0 character when counting

---
+++ basic_concepts/0x52.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: success (0).
+ */
+int main(void)
+{
+	char alphabets[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int len = strlen(alphabets), size = sizeof(alphabets);
+
+	printf("The length of alphabets is: %d\n", len);
+	printf("The size of alphebets is: %d", size);
+	return (0);
+}
