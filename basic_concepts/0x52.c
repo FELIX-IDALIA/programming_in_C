e63f666302c0599bf80da01bbd79e098236c077b
Felix Idalia <flxidalia@gmail.com>
2023-10-08 12:28:31 +0000

    File modified

--- basic_concepts/0x52.c
+++ basic_concepts/0x52.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	char alphabets[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int len = strlen(alphabets), size = sizeof(alphabets);
-
+i
 	printf("The length of alphabets is: %d\n", len);
-	printf("The size of alphebets is: %d", size);
+	printf("The size of alphebets is:   %d", size);
 	return (0);
 }
