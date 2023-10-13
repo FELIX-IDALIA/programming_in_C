f4e7ed10fc2939dfd15ef17b47c360e2b1f21ae1
Felix Idalia <flxidalia@gmail.com>
2023-10-13 19:16:10 +0000

    Getting memory address of a variable with reference operator &

---
+++ basic_concepts/0x63.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Return: success (0).
+ */
+int main(void)
+{
+	int age = 20; /* An int variable */
+
+	printf("%d", age); /*  Outputs the value of age (20) */
+	printf("%p", &age); /* Ouputs the memory address */
+	return (0);
+}
