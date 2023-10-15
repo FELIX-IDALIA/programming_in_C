d37527303dd397403c262fa30d125fdd919a0abb
Felix Idalia <flxidalia@gmail.com>
2023-10-15 04:46:57 +0000

    In the file example, myFunction() is used to print a text (the action), when it is called

---
+++ basic_concepts/0x73.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+/**
+ * myFunction - Create  function.
+ *
+ * Return: no return type.
+ */
+int myFunction(void)
+{
+	printf("I'm a Software Engineer");
+	return (0);
+}
+/**
+ * main - Calling a function
+ *
+ * Return: success(0)
+ */
+int main(void)
+{
+	myFunction();
+	return (0);
+}
