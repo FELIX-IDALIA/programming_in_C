161914e5b1aab0c9eb545d0ec9eb5be99acc5fad
Felix Idalia <flxidalia@gmail.com>
2023-10-20 12:37:53 +0300

    For code optimization, it is recommended to separate the declaration and the definition of the function

---
+++ basic_concepts/0x81.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//Function declaration
+void myFunction();
+
+int main()
+{
+	myFunction();
+	return 0;
+}
+
+//Function definition
+void myFunction()
+{
+	printf("Code to be executed");
+}
+
