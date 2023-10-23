fa6eeeb5b57d38e981e8c432ea353097da40e5da
Felix Idalia <flxidalia@gmail.com>
2023-10-23 19:34:05 +0300

    Create a structure with the name s1. You can createa a structure using the struct keyword and declare each of its members inside curly braces. To access a structure you must create a variable of it. Use the struct keyword inside the main() method, followed by the name of the structure variable.

---
+++ basic_concepts/0x93.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+// Create a structure
+struct myStructure	// Structure declaration
+{
+	int myNum;	// Member (int variable)
+	char myLetter;	// Member (char variable)
+};
+
+int main()
+{
+	struct myStructure s1;
+	return 0;
+}
