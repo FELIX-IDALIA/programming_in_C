e9e9701953d616ead610419bb17aeba9fd4c1610
Felix Idalia <flxidalia@gmail.com>
2023-10-25 10:20:43 +0300

    Modified file, added return 0

--- basic_concepts/0x98.c
+++ basic_concepts/0x98.c
@@ -20,4 +20,6 @@ int main()
 	printf("NAME:  %s\nDATE:  %d\nMONTH: %s\nYEAR:  %d\n", s1.name, s1.date, s1.month, s1.year);
 	printf("\n");
 	printf("NAME:  %s\nDATE:  %d\nMONTH: %s\nYEAR:  %d", s2.name, s2.date, s2.month, s2.year);
+
+	return 0;
 }
